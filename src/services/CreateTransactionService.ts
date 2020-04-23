f26dd5d92599a7d24dfd043f2e0ccf364e7b31de
Bruno-Signori <brunowsignori@gmail.com>
2020-04-23 15:27:35 -0300

    desafio node e typescript completo

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,16 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface requestDTO {
+
+  title: string;
+
+  value: number;
+
+  type: 'income' | 'outcome';
+}
+
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +18,24 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({title, value, type}: requestDTO): Transaction {
+    //if para valiar apenas preechimento com 'income' e 'outcome';
+    if (!['income', 'outcome'].includes(type)) {
+      throw new Error('Transações validas: income ou outcome')
+
+    }
+
+    const { total } = this.transactionsRepository.getBalance();
+    if(type === 'outcome' && total < value){
+      throw new Error("Seu saldo é de R$:" + total +
+      " FAÇA UM DEPÓSITO PARA CONTINUAR " );
+    }
+  const transaction = this.transactionsRepository.create({
+    title,
+    value,
+    type,
+  });
+  return transaction;
   }
 }
 
